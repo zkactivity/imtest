e70d47a08cc06e9fff88b8a0b2609fffb8333f5d
zk <zkactivity@gmail.com>
2018-06-24 10:52:50 -0700

    modify

--- client/client.cpp
+++ client/client.cpp
@@ -1,127 +1,59 @@
-extern "C" {
-#include <event2/http.h>
-#include <event2/http_struct.h>
-#include <event2/event.h>
-#include <event2/buffer.h>
-#include <event2/dns.h>
-#include <event2/thread.h>
-
+/***************************************************************************
+ *                                  _   _ ____  _
+ *  Project                     ___| | | |  _ \| |
+ *                             / __| | | | |_) | |
+ *                            | (__| |_| |  _ <| |___
+ *                             \___|\___/|_| \_\_____|
+ *
+ * Copyright (C) 1998 - 2015, Daniel Stenberg, <daniel@haxx.se>, et al.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution. The terms
+ * are also available at https://curl.haxx.se/docs/copyright.html.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the COPYING file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/ 
+/* <DESC>
+ * simple HTTP POST using the easy interface
+ * </DESC>
+ */ 
 #include <stdio.h>
-#include <string.h>
-#include <assert.h>
-#include <sys/queue.h>
-#include <event.h>
+#include <curl/curl.h>
+ 
+int main(void)
+{
+  CURL *curl;
+  CURLcode res;
+ 
+  /* In windows, this will init the winsock stuff */ 
+  curl_global_init(CURL_GLOBAL_ALL);
+ 
+  /* get a curl handle */ 
+  curl = curl_easy_init();
+  if(curl) {
+    /* First set the URL that is about to receive our POST. This URL can
+       just as well be a https:// URL if that is what should receive the
+       data. */ 
+    curl_easy_setopt(curl, CURLOPT_URL, "http://127.0.0.1:5000");
+    /* Now specify the POST data */ 
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel&project=curl");
+ 
+    /* Perform the request, res will get the return code */ 
+    res = curl_easy_perform(curl);
+    /* Check for errors */ 
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    /* always cleanup */ 
+    curl_easy_cleanup(curl);
+  }
+  curl_global_cleanup();
+  return 0;
 }
-
-#include <iostream>
-using namespace std;
-
-
-void RemoteReadCallback(evhttp_request * remote_req, void * arg) {
-	event_base_loopexit((event_base *)arg, NULL);
-}
-
-int ReadHeaderDoneCallback(evhttp_request * remote_req, void *arg) {
-	cout << "HTTP/1.1 " << evhttp_request_get_response_code(remote_req) << "  " <<
-		evhttp_request_get_response_code_line(remote_req) << endl;
-	evkeyvalq * headers = evhttp_request_get_input_headers(remote_req);
-	evkeyval* header;
-	TAILQ_FOREACH(header, headers, next)
-	{
-		cout << "< " << header->key << " : " << header->value << endl;
-	}
-	cout << "<" << endl;
-}
-
-void ReadChunkCallback(evhttp_request * remote_req, void * arg) {
-	char buf[4096];
-	evbuffer * evbuf = evhttp_request_get_input_buffer(remote_req);
-	int n = 0;
-	cout << "--------------> ReadChunkCallback" << endl;
-	while((n = evbuffer_remove(evbuf, buf, 4096)) > 0) {
-		cout << buf;
-	}
-}
-
-void RemoteRequestErrorCallback(evhttp_request_error err, void * arg) {
-	cout << "Remote connection closed." << endl;
-	event_base_loopexit((event_base *)arg, NULL);
-}
-
-void RemoteConnectionCloseCallback(evhttp_connection * conn, void * arg) {
-	cout << "Remote connection closed." << endl;
-	event_base_loopexit((event_base *)arg, NULL);
-}
-
-int main(int argc, char * argv[]) {
-	if (argc != 2) {
-		cout << "usage: " << argv[0] << " [url]." << endl;
-		return 1;
-	}
-	char * url = argv[1];
-	evhttp_uri * uri = evhttp_uri_parse(url);
-	if(!uri) {
-		cout << "Parsing url failed." << endl;
-		return 1;
-	}
-
-	event_base * base = event_base_new();
-	if(!base) {
-		cout << "Create event base failed." << endl;
-		return 1;
-	}
-
-	evdns_base * dnsbase = evdns_base_new(base, 1);
-	if(!dnsbase) {
-		cout << "Create dns base failed." << endl;
-	}
-	assert(dnsbase);
-
-	evhttp_request * request = evhttp_request_new(RemoteReadCallback, base);
-	evhttp_request_set_header_cb(request, ReadHeaderDoneCallback);
-	evhttp_request_set_chunked_cb(request, ReadChunkCallback);
-	evhttp_request_set_error_cb(request, RemoteRequestErrorCallback);
-
-	const char * host = evhttp_uri_get_host(uri);
-	if(!host) {
-		cout << "Parse host failed." << endl;
-		return 1;
-	}
-	
-	int port = evhttp_uri_get_port(uri);
-	if(port < 0) {
-		port = 80;
-	}
-
-	const char * request_url = url;
-	const char * path = evhttp_uri_get_path(uri);
-	if(path == NULL || strlen(path) == 0) {
-		request_url = "/";
-	}
-
-	cout << "url: " << url << endl <<
-			"host: " << host << endl <<
-			"port: " << port << endl <<
-			"path: " << path << endl <<
-			"request_url: " << request_url << endl;
-
-	evhttp_connection * conn = evhttp_connection_base_new(base, dnsbase, host, port);
-	if(!conn) {
-		cout << "Create evhttp connection failed." << endl;
-		return 1;
-	}
-
-	evhttp_connection_set_closecb(conn, RemoteConnectionCloseCallback, base);
-
-	evhttp_add_header(evhttp_request_get_output_headers(request), "Host", host);
-	evhttp_make_request(conn, request, EVHTTP_REQ_GET, request_url);
-
-	event_base_dispatch(base);
-
-	return 0;
-}
-
-
-
-
-
--- server/server.py
+++ server/server.py
@@ -1,13 +1,22 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
-@app.route('/', methods = ['POST'])
+@app.route('/', methods = ['GET', 'POST'])
 def login():
-	if request.method == 'POST':
-		#do the login
-		pass
-	else:
-		#show the login form
-		pass
+    error = None
+    if request.method == 'POST':
+        print(' The post request. ')
+        '''
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+        '''
+    else:
+        print('Other request.')
+    return 'login finished.'
 
-
+if __name__ == '__main__':
+	app.run()
